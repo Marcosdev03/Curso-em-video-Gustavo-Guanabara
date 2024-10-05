77b22ba39ddc16189eabf0f31f560f0484aa2793
marcosdev03 <sdfreitas7@gmail.com>
2024-10-05 12:38:27 -0300

    Renomeação de arquivo e marge aplicados

--- ex011.py
+++
@@ -1,4 +0,0 @@
-larg = float(input('Digite a largura:'))
-alt = float(input('Digite a altura:'))
-area = larg * alt
-print('Sua parede te a dimensão de {}x{} é sua area total de {}.'.format(larg, alt, area))
